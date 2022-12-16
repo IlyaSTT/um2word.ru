1976b74f1c181736ab37e44c36016cc3ed406242
ilya <sterlyakhin@inbox.ru>
2022-12-16 17:11:10 +0300

    Наибольшее число из трех

--- HomeWork/Task2/Program.cs
+++ HomeWork/Task2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число a:");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите число b:");
+int b = int.Parse(ReadLine()!);
+
+WriteLine("Введите число c:");
+int c = int.Parse(ReadLine()!);
+
+if (a > b)
+{
+    if (a > c)
+    {
+        WriteLine("Максимальное число: " + a);
+    }
+    else
+    {
+        WriteLine("Максимальное число: " + c);
+    }
+}
+
+else if (b > c)
+{
+   WriteLine("Максимальное число: " + b);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + c);
+}
+
+
+
+
+
+
+
+
+

4f5bbcbf0f203fa84cb4e41cb321837e92782991
ilya <sterlyakhin@inbox.ru>
2022-12-16 17:08:59 +0300

    Наибольшее число из трех

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    if (firstNumber > thirdNumber)
+    {
+        Console.WriteLine("Максимальное число: " + firstNumber);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + thirdNumber);
+    }
+}
+
+else if (secondNumber > thirdNumber)
+{
+    Console.WriteLine("Максимальное число: " + secondNumber);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + thirdNumber);
+}

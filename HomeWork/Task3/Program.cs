93d29f930291f22182264e3c4138408e16f0b195
ilya <sterlyakhin@inbox.ru>
2022-12-16 17:26:20 +0300

    корень

--- HomeWork/Task3/Program.cs
+++ HomeWork/Task3/Program.cs
@@ -5,3 +5,16 @@ WriteLine("Введите число:");
 int a = int.Parse(ReadLine()!);
 //int b = Convert.ToInt32(ReadLine());
 WriteLine($"Квадрат числа {a} равен {a*a}");
+using static System.Console;
+
+WriteLine("Введите число:");
+
+int a = int.Parse(ReadLine()!);
+//int b = Convert.ToInt32(ReadLine());
+WriteLine($"Квадрат числа {a} равен {a*a}");using static System.Console;
+
+WriteLine("Введите число:");
+
+int a = int.Parse(ReadLine()!);
+//int b = Convert.ToInt32(ReadLine());
+WriteLine($"Квадрат числа {a} равен {a*a}");
